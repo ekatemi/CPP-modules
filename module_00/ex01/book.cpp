7fa79dd23a889ed5a51b8074afe4b8301725c826
Ekaterina Mikhaylova <emikhayl@car9s6.42barcelona.com>
2024-10-25 19:56:49 +0200

    01 started

---
+++ module_00/ex01/book.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+class Contact {
+  public:
+    std::string firstName;
+    std::string lastName;
+    std::string nickName;
+    std::string darkestSecret;
+    int phoneNum;
+};
