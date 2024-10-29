74f639961640baf83953a413471cfa72fd31bfdb
Ekaterina Mikhaylova <emikhayl@car2s6.42barcelona.com>
2024-10-29 21:30:49 +0100

    check while

--- module_00/ex01/main.cpp
+++ module_00/ex01/main.cpp
@@ -12,22 +12,35 @@ int checkCmd(std::string cmd)
     return (0);
 }
 
-void getInput(std::string &input, const std::string &prompt) {
-    do {
+int getInput(std::string &input, const std::string &prompt) {
+    while (true) {
         std::cout << prompt;
         std::getline(std::cin, input);
-        if (input.empty()) {
-            std::cout << "Input cannot be empty. Please try again." << std::endl;;
+        
+        // Check for EOF
+        if (std::cin.eof()) {
+            std::cout << "\nEOF detected. Exiting program.\n";
+            return 0; // Indicate failure
         }
-    } while (input.empty());
+
+        // Check if input is empty
+        if (input.empty()) {
+            std::cout << "Input cannot be empty. Please try again." << std::endl;
+        } else {
+            return 1; // Indicate success
+        }
+    }
+    return 0;
 }
 
 void fillData(std::string data[5]) {
-    getInput(data[0], "Enter first name: ");
-    getInput(data[1], "Enter last name: ");
-    getInput(data[2], "Enter nickname: ");
-    getInput(data[3], "Enter darkest secret: ");
-    getInput(data[4], "Enter phone number: ");
+    if (!getInput(data[0], "Enter first name: ") ||
+        !getInput(data[1], "Enter last name: ") ||
+        !getInput(data[2], "Enter nickname: ") ||
+        !getInput(data[3], "Enter darkest secret: ") ||
+        !getInput(data[4], "Enter phone number: ")) {
+        return; // Exit fillData if any getInput call fails
+    }
 }
 
 int main()
@@ -46,6 +59,10 @@ int main()
     {
         std::cout << "Enter the command: ";
         std::getline(std::cin, cmd);
+        if (std::cin.eof()) {
+            return 1; // Return to the calling function
+        }
+       
         if (cmd == "EXIT")
             break;
         else if (cmd == "ADD") {
@@ -57,13 +74,16 @@ int main()
             while (42) { // Infinite loop
                 std::cout << "Enter the index of contact (0-7): ";
                 std::getline(std::cin, index);
+                if (std::cin.eof()) {
+                        return 1; // Return to the calling function
+                }      
                 // Check if the index is valid
-                if (std::stoi(index) >= 0 && std::stoi(index) <= 7) {
+                if (std::isdigit(index[0]) && index[0] - '0' >= 0 && index[0] - '0' <= 8) {
                     break;
                 }
                 std::cout << "Invalid index." << std::endl;
             }
-            MyBook.searchContact(std::stoi(index));
+            MyBook.searchContact(index[0] - '0');
         }
         else
             std::cout << "Wrong command, try again!\n";
