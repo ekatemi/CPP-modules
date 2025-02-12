34d50dbac797f5e67a5861b7de3d01ce043e3e08
Ekaterina Mikhaylova <emikhayl@cbr11s1.42barcelona.com>
2025-02-12 22:15:55 +0100

    fixed main

--- module_04/ex_01/main.cpp
+++ module_04/ex_01/main.cpp
@@ -18,7 +18,25 @@ i->makeSound(); //will output the cat sound!
 std::cout << "will output the dog sound!\n";
 j->makeSound(); //will output the dog sound!
 
+const int size = 5;
+Animal* arr[size];
 
+for (int i = 0; i < size; i++)
+{
+    if(i < size/2)
+        arr[i] = new Dog();
+    else
+        arr[i] = new Cat();
+
+}
+
+for (int i = 0; i < size; i++) {
+    arr[i]->makeSound();
+}
+
+for (int i = 0; i < size; i++) {
+    delete arr[i];
+}
 
 delete i;
 delete j;
