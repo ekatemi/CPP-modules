2c66feb3018b9142f296ce212c8cad56ee3248c2
ekatemi <100319631+ekatemi@users.noreply.github.com>
2025-04-24 18:09:29 +0200

    00

--- module_05/ex00/Bureaucrat.cpp
+++ module_05/ex00/Bureaucrat.cpp
@@ -1,12 +1,33 @@
 #include "Bureaucrat.hpp"
 
+GradeTooHighException::GradeTooHighException() : _error("Grade too high") {};
+std::string GradeTooHighException::getError() { return _error; };
+
+GradeTooLowException::GradeTooLowException() : _error("Grade too low") {};
+std::string GradeTooLowException::getError() { return _error; };
+
 Bureaucrat::Bureaucrat() : _name("default"), _grade(1) {};
 
-Bureaucrat::Bureaucrat(const std::string &name, const unsigned int grade) : _name(name) {
+Bureaucrat::Bureaucrat(const std::string &name, const int grade) : _name(name), _grade(grade)
+{
+    if (grade < 1)
+        throw GradeTooHighException();
+    else if (grade > 150)
+        throw GradeTooLowException();
+};
 
-                                                                            };
-Bureaucrat::Bureaucrat(const Bureaucrat &src) {};
-Bureaucrat &Bureaucrat::operator=(const Bureaucrat &src) {};
+Bureaucrat::Bureaucrat(const Bureaucrat &src) : _name(src._name), _grade(src._grade) {};
+
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &src)
+{
+    if (this != &src)
+    {
+        _name = src._name;
+        _grade = src._grade;
+    }
+    return *this;
+};
+
 Bureaucrat::~Bureaucrat() {};
 
 std::string Bureaucrat::getName()
--- module_05/ex00/Bureaucrat.hpp
+++ module_05/ex00/Bureaucrat.hpp
@@ -2,15 +2,36 @@
 #define BUREAUCRAT_HPP
 
 #include <iostream>
+
+class GradeTooHighException
+{
+private:
+    std::string _error;
+
+public:
+    GradeTooHighException();
+    std::string getError();
+};
+
+class GradeTooLowException
+{
+private:
+    std::string _error;
+
+public:
+    GradeTooLowException();
+    std::string getError();
+};
+
 class Bureaucrat
 {
 private:
     const std::string _name;
-    unsigned int _grade; // ranges from 1(highest) to 150
+    int _grade; // ranges from 1(highest) to 150
 
 public:
     Bureaucrat();
-    Bureaucrat(const std::string &name, const unsigned int grade);
+    Bureaucrat(const std::string &name, const int grade);
     Bureaucrat(const Bureaucrat &src);
     Bureaucrat &operator=(const Bureaucrat &src);
     ~Bureaucrat();
