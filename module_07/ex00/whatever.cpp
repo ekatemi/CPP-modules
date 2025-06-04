72609e91c0a16976fc96d8f488a69d9587075c3a
ekatemi <100319631+ekatemi@users.noreply.github.com>
2025-06-04 18:06:10 +0200

    07

---
+++ module_07/ex00/whatever.cpp
@@ -0,0 +1,9 @@
+#include "whatever.hpp"
+
+void iter(T1 *arr, size_t len, T2 fun)
+{
+    for (size_t i = 0; i < length; ++i)
+    {
+        func(array[i]);
+    }
+}
--- module_07/ex00/whatever.hpp
+++ module_07/ex00/whatever.hpp
@@ -1,4 +1,17 @@
 #ifndef WHATEVER_HPP
 #define HEADER_HPP
+#include <iostream>
+
+template <typename T1, typename T2, typename T3>
+
+// void iter(T1 *a, size_t len, T2 fun);
+
+void iter(T1 *arr, size_t len, T2 fun)
+{
+    for (size_t i = 0; i < length; ++i)
+    {
+        func(T3 a, T3 b);
+    }
+}
 
 #endif
