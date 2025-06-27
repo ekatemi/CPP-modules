5de3e1692dec29e3ced4ae8a18220512f56409dd
Ekaterina Mikhaylova <emikhayl@cbr14s2.42barcelona.com>
2025-06-27 23:01:29 +0200

    02

--- module_08/ex02/MutantStack.hpp
+++ module_08/ex02/MutantStack.hpp
@@ -28,15 +28,19 @@ public:
     // iterators
     // std::iterator it;
     typedef typename std::stack<T>::container_type::iterator iterator;
+    typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+    typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+    typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
     iterator begin() { return this->c.begin(); }
     iterator end() { return this->c.end(); }
-    const iterator begin() const { return this->c.begin(); }
-    const iterator end() const { return this->c.end(); }
+    const_iterator begin() const { return this->c.begin(); }
+    const_iterator end() const { return this->c.end(); }
 
-    iterator rbegin() { return this->c.rbegin(); }
-    iterator rend() { return this->c.rend(); }
-    const iterator rbegin() const { return this->c.rbegin(); }
-    const iterator rend() const { return this->c.rend(); }
+
+    reverse_iterator rbegin() { return this->c.rbegin(); }
+    reverse_iterator rend() { return this->c.rend(); }
+    const_reverse_iterator rbegin() const { return this->c.rbegin(); }
+    const_reverse_iterator rend() const { return this->c.rend(); }
 };
 // class MutantStack : public std::stack<T>
 // {
--- module_08/ex02/main.cpp
+++ module_08/ex02/main.cpp
@@ -5,31 +5,80 @@
 
 int main()
 {
+    /*empty size back push_back pop_back*/
+    
+    std::cout << "\033[32m*** MUTANT ***\033[0m" << std::endl;
+    MutantStack<int> mstack;
+    if(mstack.empty()){
+        std::cout << "container empty" << std::endl;
+    }
 
-    // MutantStack<int> mstack;
-    std::list<int> mstack;
-    mstack.push_back(5);
-    mstack.push_back(17);
-    // std::cout << mstack.top() << std::endl;
-    std::cout << mstack.back() << std::endl;
-    mstack.pop_front();
-    std::cout << mstack.size() << std::endl;
-    mstack.push_back(3);
-    mstack.push_back(5);
-    mstack.push_back(737);
+    mstack.push(5); //adds at the top -> 5
+    mstack.push(17); // -> 5 17
+    std::cout << mstack.top() << std::endl; //shows last added on top  -> shows 17 ---------
+    mstack.pop(); //removes 1st element  -> removes 17
+    std::cout << mstack.size() << std::endl; //number of elements at stack  -> 1 ----------
+    mstack.push(3); //  -> 5 3
+    mstack.push(5); //  -> 5 3 5
+    mstack.push(737); //  -> 5 3 5 737
     //[...]
-    mstack.push_back(0);
-    std::list<int>::iterator it = mstack.end();
-    std::list<int>::iterator ite = mstack.begin();
-    // MutantStack<int>::iterator ite = mstack.end();
-    ++it;
-    --it;
+    mstack.push(0); // -> 5 3 5 737 0 
+    std::cout << "\033[32m reverse iterator \033[0m" << std::endl;
+    MutantStack<int>::reverse_iterator beg = mstack.rbegin();
+    MutantStack<int>::reverse_iterator end = mstack.rend();
+    while (beg != end)
+    {
+        std::cout << *beg << std::endl;
+        ++beg;
+    }
+    
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+    ++it; 
+    --it; //same place
+    std::cout << "\033[32m forward iterator \033[0m" << std::endl;
     while (it != ite)
     {
-        std::cout << "in list " << *it << std::endl;
-        ++it;
+    std::cout << *it << std::endl; // -> 5 3 5 737 0 --------------
+    ++it;
     }
-    // std::stack<int> s(mstack);
-    std::list<int> s(mstack);
+    std::stack<int> s(mstack);
+    
+    
+    std::cout << "\033[32m*** LIST ***\033[0m" << std::endl;
+    std::list<int> lstack;
+    if(lstack.empty()){
+        std::cout << "container empty" << std::endl;
+    }
+    lstack.push_back(5); // -> 5
+    lstack.push_back(17); // -> 5 17
+    std::cout << lstack.back() << std::endl; // ------------------------
+    lstack.pop_back(); // -> removes 17
+    std::cout << lstack.size() << std::endl; // -> 1 -------------------
+    lstack.push_back(3);
+    lstack.push_back(5);
+    lstack.push_back(737);
+    //[...]
+    lstack.push_back(0);
+    std::cout << "\033[32m reverse iterator \033[0m" << std::endl;
+    std::list<int>::reverse_iterator rit_b = lstack.rbegin();
+    std::list<int>::reverse_iterator rit_e = lstack.rend();
+    while (rit_b != rit_e)
+    {
+        std::cout << *rit_b << std::endl;
+        ++rit_b;
+    }
+
+    std::list<int>::iterator it_l = lstack.begin();
+    std::list<int>::iterator ite_l = lstack.end();
+    ++it;
+    --it;
+    std::cout << "\033[32m forward iterator \033[0m" << std::endl;
+    while (it_l != ite_l)
+    {
+        std::cout << *it_l << std::endl;
+        ++it_l;
+    }
+    std::list<int> l(lstack);
     return 0;
 }
