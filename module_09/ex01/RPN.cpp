bb57cea2f344cfe212369586563fa1a8f9adb774
ekatemi <100319631+ekatemi@users.noreply.github.com>
2025-08-20 16:35:37 +0200

    calc

--- module_09/ex01/RPN.cpp
+++ module_09/ex01/RPN.cpp
@@ -1,32 +1,63 @@
 #include "RPN.hpp"
 
 RPN::RPN() : myStack() {}
-RPN::RPN(const RPN &src) : myStack(src.myStack){}
-RPN &RPN::operator=(const RPN &src){
+RPN::RPN(const RPN &src) : myStack(src.myStack) {}
+RPN &RPN::operator=(const RPN &src)
+{
     if (this != &src)
         myStack = src.myStack;
     return *this;
 }
 RPN::~RPN() {}
 
-//methods
-int RPN::calcRes(const std::string str) {
+bool isOp(char c)
+{
+    return (c == '+' || c == '-' || c == '*' ||
+            c == '/');
+}
+
+bool charOk(char c)
+{
+    return (std::isdigit(static_cast<unsigned char>(c)) || isOp(c));
+}
+
+bool isSpace(char c)
+{
+    return (c == ' ' || c == '\t');
+}
+
+void RPN::fillStack(std::string str)
+{
+    for (int i = 0; i < str.size(); i++)
+    {
+        if (isSpace(str[i]))
+            i++;
+        else if (isdigit(str[i]))
+            myStack.push(str[i]);
+        else if (isOp)
+            calcRes(); // TODO
+    }
+    if (myStack.size() !=)
+}
+
+// methods
+int RPN::calcRes(const std::string str)
+{
     int num;
     int t1;
     int t2;
-    for(int i = 0; i < str.size(); i++) {
+    for (int i = 0; i < str.size(); i++)
+    {
         if (isdigit(static_cast<unsigned char>(str[i])))
             num = str[i] - '0';
         myStack.push(num);
-        else if (str[i] == '-' || str[i] == '-' || str[i] == '*' || str[i] == '/') {
+        else if (str[i] == '-' || str[i] == '-' || str[i] == '*' || str[i] == '/')
+        {
             int t1 = st.top(); // get top element
             st.pop();
             int t2 = st.top(); // get top element
             st.pop();
-            num = 
+            num =
         }
     }
 }
-
-
-
--- module_09/ex01/RPN.hpp
+++ module_09/ex01/RPN.hpp
@@ -1,23 +1,25 @@
 #ifndef RPN_HPP
-# define RPN_HPP
+#define RPN_HPP
 
-#include <iostream>       // std::cout
-#include <stack>          // std::stack
+#include <iostream> // std::cout
+#include <stack>    // std::stack
 
-class RPN {
-    private:
-        std::stack<int> myStack;
-    public:
-        RPN();
-        RPN(const RPN &src);
-        RPN &operator=(const RPN &src);
-        ~RPN();
+class RPN
+{
+private:
+    std::stack<int> myStack;
 
-        //methods
-        int calcRes(std::string str);
+public:
+    RPN();
+    RPN(const RPN &src);
+    RPN &operator=(const RPN &src);
+    ~RPN();
+
+    // methods
+    void fillStack(std::string str);
+    int calcRes(std::string str);
 };
-
 
 #endif
 
-// + - / * 
+// + - / *
